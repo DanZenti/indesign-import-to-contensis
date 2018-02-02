8f2c8da5441148739d4275408c03f9fbff723d80
DanBadham <danbadham@googlemail.com>
2018-02-02 12:57:11 +0000

    rename booklet

--- old/ProgramBACKUP.cs
+++ old/ProgramBACKUP.cs
@@ -24,21 +24,22 @@ namespace ParsingXml
         {
 
             // Create the management client
-            var client = ManagementClient.Create("https://cms-danb-dev.cloud.contensis.com",
-                "f39cc495-2640-4f55-a39c-07b8d49692f0",
-                "34bd681443474c3fbb13d361ab9a04c6-a7c40152960f41ef98cc3ecd4c1dea28-f3963186a3a94ca0a19b327a29d5a2b7");
+            var client = ManagementClient.Create("**ADD CMS URL HERE**",
+                "**ADD THE CLIENT ID FROM YOUR CONTENSIS PROJECT**",
+                "**ADD THE SHARED SECRET FROM YOUR CONTENSIS PROJECT**");
 
             // Get the project
             var project = client.Projects.Get("Website");
 
             // Load the XML file into an XElement
-            XElement doc = XElement.Load(@"C:\Training\ManagementAPI\dotnet-xml-import\XMLImport\finalXML.xml");
+            XElement doc = XElement.Load(@"C:\Training\ManagementAPI\dotnet-xml-import\XMLImport\booklet.xml");
             // Navigate the XElement to create the stories
             IEnumerable<Story> stories = (from x in doc.Descendants("Stories").Elements("Story")
                                             select new Story
                                             {
                                                 Title = x.Element("SectionHeading").Value,
-                                                Introduction = x.Element("SectionIntroduction").Value
+                                                Introduction = x.Element("SectionIntroduction").Value,
+                                                FeatureImage = x.Element("FeatureImage").Attribute("href").Value
                                             }).ToList();
             // Local testing in console
             //Console.WriteLine("Story title: " + stories.First().Title);
@@ -62,7 +63,7 @@ namespace ParsingXml
             // Set each field value
             storyEntry.Set("title", story.Title);
             storyEntry.Set("subtitle", story.Introduction);
-            //storyEntry.Set("featureImage", story.FeatureImage);
+            storyEntry.Set("featureImage", story.FeatureImage);
 
             // Save and publish the story
             storyEntry.Save();
